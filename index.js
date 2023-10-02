1bad96d26589b40add1873a9b1fa602dfc4c6861
Will <willb@openplay.co.uk>
2023-10-02 16:35:23 +0100

    removed annotation generation

--- index.js
+++ index.js
@@ -23,24 +23,6 @@ async function run() {
       for (const item of jsonData) {
         core.summary.addDetails(item.title);
       }
-
-      if (annotations.length > 0) {
-        const octokit = github.getOctokit(core.getInput('github_token'));
-
-        try {     
-          annotations.forEach(element => {
-            core.warning(element.title + " is marked as flaky, consider rerunning");
-          });
-        } catch (error) {
-          console.error('API Error:', error.message);
-          core.setFailed('Failed to create annotations.');
-        }
-      } else {
-        console.log('No annotations to create.');
-      }
-    } else {
-      console.log(jsonData);
-      core.setFailed('No json content available');
     }
   } catch (error) {
     core.setFailed(error.message);
